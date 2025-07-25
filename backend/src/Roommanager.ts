c622818bdbe9009cf907560a028e5755b3abddd4
vineet <155095640+0123vineet@users.noreply.github.com>
2025-07-26 01:30:53 +0530

    fix disconnetion thing

--- backend/src/Roommanager.ts
+++ backend/src/Roommanager.ts
@@ -1,6 +1,6 @@
 import { WebSocket } from "ws";
 import { call } from "./call";
-import { v4 as uuidv4 } from 'uuid';
+import {  v4 as uuidv4 } from 'uuid';
 
 export class Roommanager {
   private pendinguser: WebSocket[] = [];
